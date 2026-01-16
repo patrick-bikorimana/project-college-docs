f3fe62b5f0e750b29d3ade34e34c1a135dd280e1
Patrick <patrick.bikorimana.tech@gmail.com>
2026-01-16 21:05:11 +0100

    first commit

---
+++ src/main/java/be/ephec/project_college_docs/ProjectCollegeDocsApplication.java
@@ -0,0 +1,13 @@
+package be.ephec.project_college_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCollegeDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCollegeDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ephec/project_college_docs/ProjectCollegeDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ephec.project_college_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCollegeDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
