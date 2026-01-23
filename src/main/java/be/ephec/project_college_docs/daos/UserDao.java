5c099ba288627b01510f26ffa25c52b1fbe7c534
Patrick <patrick.bikorimana.tech@gmail.com>
2026-01-23 19:12:43 +0100

    Daos implementation for login

---
+++ src/main/java/be/ephec/project_college_docs/daos/UserDao.java
@@ -0,0 +1,47 @@
+package be.ephec.project_college_docs.daos;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@ToString
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class UserDao extends Auditable{
+    @Column(unique = true,updatable = false, nullable = false)
+    private String userId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true,updatable = false, nullable = false)
+    private String email;
+    private Integer loginAttempts;
+    private LocalDateTime lastLogin;
+    private String phone;
+    private String bio;
+    private String imageUrl;
+    private boolean isAccountNotExpired;
+    private boolean isAccountNotLocked;
+    private boolean isAccountEnabled;
+    private boolean mfaEnabled;
+    @JsonIgnore
+    private String qrCodeSecret;
+    @Column(columnDefinition = "TEXT") //more characters than default varchar
+    private String qrCodeUri;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_role",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private RoleDao role;
+}
--- src/main/java/be/ephec/project_college_docs/enumerations/Authority.java
+++ src/main/java/be/ephec/project_college_docs/enumerations/Authority.java
@@ -1,10 +1,10 @@
 package be.ephec.project_college_docs.enumerations;
-/**
- * This Enum that strictly defines the available roles in the application and maps them to specific sets of permissions (authorities).
- */
 
 import static be.ephec.project_college_docs.constants.Constant.*;
 
+/**
+ * This Enum that strictly defines the available roles in the application and maps them to specific sets of permissions (authorities).
+ */
 public enum Authority {
     USER(USER_AUTHORITIES),
     ADMIN(ADMIN_AUTHORITIES),
